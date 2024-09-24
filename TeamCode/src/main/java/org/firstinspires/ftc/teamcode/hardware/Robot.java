265618e3d3bd0505621e1e3629fc90b58f20ce3f
--global <131030642+YuvalW-Dev@users.noreply.github.com>
2024-09-24 19:48:34 -0400

    Started drivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+    public DriveTrain driveTrain = new DriveTrain();
+
+    public void init(HardwareMap hwMap) {
+        driveTrain.init(hwMap);
+    }
+}
