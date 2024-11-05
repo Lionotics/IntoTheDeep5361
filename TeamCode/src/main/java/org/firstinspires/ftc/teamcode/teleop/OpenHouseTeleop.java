3c3e47a81daa597ee1efb47a2f5cce64fdf74400
YuvalW-Dev <131030642+YuvalW-Dev@users.noreply.github.com>
2024-11-05 18:13:07 -0500

    Open House Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/OpenHouseTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/OpenHouseTeleop.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-public class OpenHouseTeleop {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+
+@TeleOp(name="OpenHouseTeleop")
+public class OpenHouseTeleop extends LinearOpMode {
+
+    Robot robot = new Robot();
+    Gamepad gp1 = new Gamepad();
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            robot.driveTrain.drive(gp1.left_stick_y, gp1.left_stick_x, gp1.right_stick_x);
+        }
+    }
 }
