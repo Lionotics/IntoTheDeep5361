b708c7333b02a4dcdeb3fc62b11593ef38720580
YuvalW-Dev <131030642+YuvalW-Dev@users.noreply.github.com>
2024-11-15 14:00:27 -0500

    Set up intakeTesting and started rewriting the code for slides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Slides.java
@@ -11,111 +11,26 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class Slides {
 
     private DcMotor differentialRight, differentialLeft;
+    private final double HOLD_VERTICAL_SLIDE_POWER = 0.1d;
 
-    //TODO: Incorporate this into the class
-    /*
-    private DcMotor differentialRight, differentialLeft;
-
-    @Override
-    public void runOpMode() {
-        differentialRight = hardwareMap.dcMotor.get("differentialRight");
-        differentialLeft = hardwareMap.dcMotor.get("differentialLeft");
-
-        differentialLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        differentialRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-            if (gamepad1.dpad_up) {
-                differentialRight.setPower(-.7);
-                differentialLeft.setPower(.7);
-            } else if (gamepad1.dpad_down) {
-                differentialRight.setPower(.7);
-                differentialLeft.setPower(-.7);
-            } else if (gamepad1.dpad_left) {
-                differentialRight.setPower(-.7);
-                differentialLeft.setPower(-.7);
-            } else if (gamepad1.dpad_right) {
-                differentialRight.setPower(.7);
-                differentialLeft.setPower(.7);
-            } else {
-                differentialRight.setPower(-0.1);
-                differentialLeft.setPower(0.1);
-            }
-        }*/
-    double FIRST_POS = 1000;
-    double SECOND_POS = 2000;
-    double HOLD =.03;
-    SlideState state = SlideState.BOTTOM;
-    SlideState goalState = SlideState.BOTTOM;
-
-
-    public void loop() {
-        switch (state){
-            case UP:
-                slide.setPower(1);
-                if(goalState == SlideState.FIRST && slide.getCurrentPosition() >= FIRST_POS){
-                    state = SlideState.FIRST;
-                } else if (goalState == SlideState.SECOND && slide.getCurrentPosition() >= SECOND_POS){
-                    state = SlideState.SECOND;
-                }
-            case DOWN:
-                slide.setPower(-1);
-                if(goalState == SlideState.FIRST && slide.getCurrentPosition() <= FIRST_POS){
-                    state = SlideState.FIRST;
-                } else if (goalState == SlideState.BOTTOM && slide.getCurrentPosition() <= 0){
-                    state = SlideState.BOTTOM;
-                }
-                break;
-            default:
-                slide.setPower(HOLD);
-        }
-    }
-
-    public void upHandler() {
-        switch(state){
-            case BOTTOM:
-                state = SlideState.UP;
-                goalState = SlideState.FIRST;
-                break;
-            case FIRST:
-                state = SlideState.UP;
-                goalState = SlideState.SECOND;
-                break;
-            default:
-                break;
-        }
-    }
-    public void downHandler() {
-        switch(state){
-            case FIRST:
-                state = SlideState.DOWN;
-                goalState = SlideState.BOTTOM;
-                break;
-            case SECOND:
-                state = SlideState.DOWN;
-                goalState = SlideState.FIRST;
-                break;
-            default:
-                break;
-        }
-    }
-
-    public enum SlideState {
-        UP,
-        DOWN,
-        BOTTOM,
-        FIRST,
-        SECOND
-    }
-
-    DcMotor slide;
     public void init(HardwareMap hwMap) {
         differentialRight = hwMap.dcMotor.get("differentialRight");
         differentialLeft = hwMap.dcMotor.get("differentialLeft");
 
         differentialLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         differentialRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void verticalSlide(double power) {
+        differentialRight.setPower(-power);
+        differentialLeft.setPower(power);
+    }
+    public void horizontalSlide(double power) {
+        differentialRight.setPower(power);
+        differentialLeft.setPower(power);
+    }
+    public void hold() {
+        differentialRight.setPower(-HOLD_VERTICAL_SLIDE_POWER);
+        differentialLeft.setPower(HOLD_VERTICAL_SLIDE_POWER);
     }
 }
