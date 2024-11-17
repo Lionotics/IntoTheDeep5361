b83d45b5dee1c370c11240de46f825d3faade547
YuvalW-Dev <131030642+YuvalW-Dev@users.noreply.github.com>
2024-11-17 17:45:04 -0500

    Finished Intake w/ State Machine and Tuning, started End Effector (minor changes to Drive Train)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/EndEffector.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+public class EndEffector {
+
+    public static double CLAW_CLOSE = 0, CLAW_OPEN = 0;
+    public static double ROTATE_UP = 0, ROTATE_DOWN = 0;
+
+    Servo claw, wrist;
+
+    public void init(HardwareMap hwMap) {
+        claw = hwMap.get(Servo.class, "endEffectorClaw");
+        wrist = hwMap.get(Servo.class, "endEffectorPivot");
+    }
+
+    private void setClaw(double pos) {claw.setPosition(pos);}
+    private void setWrist(double pos) {wrist.setPosition(pos);}
+
+    public void close() {
+        setClaw(CLAW_CLOSE);
+    }
+
+    public void open() {
+        setClaw(CLAW_OPEN);
+    }
+
+    public void rotateUp() {
+        setWrist(ROTATE_UP);
+    }
+
+    public void rotateDown() {
+        setWrist(ROTATE_DOWN);
+    }
+}
