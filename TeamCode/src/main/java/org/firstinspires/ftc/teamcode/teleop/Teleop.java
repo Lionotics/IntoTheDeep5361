75bea8571b62542c630230d33a526bd61e2200ac
YuvalW-Dev <131030642+YuvalW-Dev@users.noreply.github.com>
2024-11-24 10:51:33 -0500

    Bug Fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
@@ -7,13 +7,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.hardware.Slides;
 import org.firstinspires.ftc.teamcode.helpers.GamepadEx;
 
 @TeleOp(name = "Teleop", group = "Teleop")
 public class Teleop extends LinearOpMode {
 
     Robot robot = new Robot();
-    GamepadEx gp1, gp2;
+    GamepadEx gp1 = new GamepadEx(), gp2 = new GamepadEx();
 
 
     @Override
@@ -23,9 +24,6 @@ public class Teleop extends LinearOpMode {
         robot.init(hardwareMap);
         robot.intake.init(); //TODO: Add at the end of auton, remove this later
         while (opModeIsActive()) {
-            gp1.update(gamepad1);
-            gp2.update(gamepad2);
-
             if (gp1.rightBumper.isNewlyPressed()) {
                 robot.intake.incrementState();
             } else if (gp1.leftBumper.isNewlyPressed()) {
@@ -44,6 +42,7 @@ public class Teleop extends LinearOpMode {
 
             robot.driveTrain.driveRobotCentric(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
             gp1.update(gamepad1);
+            gp2.update(gamepad2);
 
             telemetry.addData("Index: ", robot.intake.currentState.name());
             telemetry.addData("Horizontal: ", robot.slides.getHorizontalPos());
