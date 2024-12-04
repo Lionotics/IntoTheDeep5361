efb16f35278cc7e569bea4038671158f329e41bd
daniel-farkas-dev <54506760+daniel-farkas-dev@users.noreply.github.com>
2024-12-03 20:04:16 -0500

    Fixed auto slides

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,11 +14,13 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16.4)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(90), 16.4)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(47, 62, Math.toRadians(180)))
-                .stopAndAdd(new SleepAction(4)) // Raise the slides, open the claw
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(38, 63, Math.toRadians(180)))
+                .strafeToConstantHeading(new Vector2d(46, 60))
+                .splineToSplineHeading(new Pose2d(54.5, 51.5, Math.toRadians(225)), Math.toRadians(0))
+                /*.stopAndAdd(new SleepAction(4)) // Raise the slides, open the claw
                 .splineToSplineHeading(new Pose2d(58.4, 35.5, Math.toRadians(-90)), Math.toRadians(0))
                 .stopAndAdd(new SleepAction(1)) // Pick up middle sample
                 .splineToSplineHeading(new Pose2d(58.5, 62, Math.toRadians(235)), 0)
@@ -31,7 +33,7 @@ public class MeepMeepTesting {
                 .stopAndAdd(new SleepAction(1)) // Pick up right sample
                 .strafeTo(new Vector2d(52, 45)) // Get away from the wall
                 .splineToSplineHeading(new Pose2d(58.5, 62, Math.toRadians(235)), 0)
-                .stopAndAdd(new SleepAction(1)) // Drop right sample
+                .stopAndAdd(new SleepAction(1)) // Drop right sample*/
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_LIGHT)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlueBucket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlueBucket.java
@@ -1,15 +1,22 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.hardware.Slides;
 
 @Autonomous(name = "Auto Blue Bucket", group = "Auto")
 public class AutoBlueBucket extends LinearOpMode {
@@ -17,16 +24,45 @@ public class AutoBlueBucket extends LinearOpMode {
     double delay = 0; // seconds
     boolean park = true;
     boolean hang = false;
+    Pose2d initialPose = new Pose2d(38, 63.5, Math.toRadians(90));
+    MecanumDrive drive;
+    int loop = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d initialPose = new Pose2d(47, 62, Math.toRadians(180));
-        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
-
-        //TrajectoryActionBuilder builder = drive.trajectoryBuilder(initialPose, true);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        drive = new MecanumDrive(hardwareMap, initialPose);
+        TrajectoryActionBuilder path1 = drive.actionBuilder(initialPose)
+                .strafeToConstantHeading(new Vector2d(46, 57))
+                .splineToSplineHeading(new Pose2d(51.5, 51.5, Math.toRadians(225)), Math.toRadians(0));
+        Action startToBucket = path1.build();
         robot.init(hardwareMap);
+        robot.ee.close();
         // TODO: Read inputs from gamepad 1
         waitForStart();
-        Actions.runBlocking(new SequentialAction(new SleepAction(delay), new ParallelAction(robot.intake.hoverIntake())));
+        Actions.runBlocking(
+                new SequentialAction(
+                        new SleepAction(delay),
+                        new InstantAction(() -> {
+                            telemetry.addData("RR IMU", Math.toDegrees(drive.pose.heading.toDouble()));
+                            telemetry.addData("RR X", drive.pose.position.x);
+                            telemetry.addData("RR Y", drive.pose.position.y);
+                        }),
+                        startToBucket, //TODO: Why does it turn ccw before moving?
+                        new InstantAction(() -> {
+                            telemetry.addData("RR IMU", Math.toDegrees(drive.pose.heading.toDouble()));
+                            telemetry.addData("RR X", drive.pose.position.x);
+                            telemetry.addData("RR Y", drive.pose.position.y);
+                        }),
+                        robot.slides.slidesMoveTo(Slides.LiftPositions.TOP_BUCKET),
+                        robot.ee.rotateUpWrist(),
+                        new SleepAction(1),
+                        robot.ee.openClaw(),
+                        new SleepAction(.5),
+                        robot.ee.rotateDownWrist(),
+                        new SleepAction(.5),
+                        robot.slides.slidesMoveTo(Slides.LiftPositions.BOTTOM)
+                )
+        );
     }
 }
