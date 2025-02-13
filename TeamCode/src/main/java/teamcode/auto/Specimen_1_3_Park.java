65660ab631bedf618bcbcacbd2f3d0c938a13377
YuvalW-Dev <131030642+YuvalW-Dev@users.noreply.github.com>
2025-02-13 12:35:29 -0500

    Switched handler for multi-threading

--- TeamCode/src/main/java/teamcode/auto/Specimen_1_3_Park.java
+++ TeamCode/src/main/java/teamcode/auto/Specimen_1_3_Park.java
@@ -11,7 +11,9 @@ import com.pedropathing.util.Constants;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
@@ -24,13 +26,13 @@ import teamcode.hardware.VSlides;
 
 @Autonomous(name = "Specimen 1+3 Park", group = "Red")
 public class Specimen_1_3_Park extends OpMode {
-    private static final Object lock = new Object();
+    private static ExecutorService executor = Executors.newSingleThreadExecutor();
     public static Pose startPose = new Pose(135.75, 88, Math.toRadians(180));
     public static Pose botSample = new Pose(115.75, 106, Math.toRadians(140));
-    public static Pose midSample = new Pose(115.75, 117, Math.toRadians(135));
-    public static Pose topSample = new Pose(115.75, 126, Math.toRadians(130));
     public static Pose botDrop = new Pose(115.75, 106, Math.toRadians(55));
+    public static Pose midSample = new Pose(115.75, 117, Math.toRadians(135));
     public static Pose midDrop = new Pose(115.75, 117, Math.toRadians(55));
+    public static Pose topSample = new Pose(115.75, 126, Math.toRadians(130));
     public static Pose topDrop = new Pose(115.75, 126, Math.toRadians(55));
     public static Point topControl = new Point(115.75, 117, Point.CARTESIAN);
     private static int pathState = 0;
@@ -41,83 +43,77 @@ public class Specimen_1_3_Park extends OpMode {
     private PathChain start2bot, bot2drop, drop2mid, mid2drop, drop2top, top2drop;
     private Intake intake;
 
-    private final Thread pickUpBlock = new Thread() {
-        @Override
-        public void run() {
+    private Runnable pickUpBlock() {
+        return () -> {
             try {
                 robot.transfer.switchToSample();
-                Thread.sleep(1000);
-                Log.d("Teamcode", "pub 1 - Expected: BARRIER; Actual: " + robot.transfer.stateMachine.getCurrentState());
+                Log.d("Teamcode", "pub 1 - Expected: BARRIER; Actual: " +
+                        robot.transfer.stateMachine.getCurrentState());
                 robot.transfer.next();
                 intake.turnWristManualLeft();
-                Log.d("Teamcode", "pub 2 - Expected: HOVERG; Actual: " + robot.transfer.stateMachine.getCurrentState());
+                Log.d("Teamcode", "pub 2 - Expected: HOVERG; Actual: " +
+                        robot.transfer.stateMachine.getCurrentState());
                 Thread.sleep(500);
                 robot.transfer.next();
-                Log.d("Teamcode", "pub 3 - Expected: GRABG; Actual: " + robot.transfer.stateMachine.getCurrentState());
+                Log.d("Teamcode", "pub 3 - Expected: GRABG; Actual: " +
+                        robot.transfer.stateMachine.getCurrentState());
                 Thread.sleep(500);
-                synchronized (lock) {
-                    Log.i("Teamcode", "pickUpBlock finished, notifying main thread");
-                    lock.notify();
-                }
             } catch (InterruptedException err) {
-                Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
+                Log.e("Teamcode", "Thread failed; Interrupting Thread...");
+                Thread.currentThread().interrupt();
             }
-        }
-    }, dropBlock = new Thread() {
-        @Override
-        public void run() {
+        };
+    };
+    private Runnable dropBlock() {
+        return () -> {
             try {
                 Thread.sleep(500);
                 robot.transfer.switchToSpecimen();
-                Log.d("Teamcode", "dp 1 - Expected: HOVERW; Actual: " + robot.transfer.stateMachine.getCurrentState());
+                Log.d("Teamcode", "dp 1 - Expected: HOVERW; Actual: " +
+                        robot.transfer.stateMachine.getCurrentState());
                 Thread.sleep(250);
-                synchronized (lock) {
-                    Log.i("Teamcode", "dropBlock finished, notifying main thread");
-                    lock.notify();
-                }
             } catch (InterruptedException err) {
-                Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
+                Log.e("Teamcode", "Thread failed; Interrupting Thread...");
+                Thread.currentThread().interrupt();
             }
-        }
-    }, placeOnBar = new Thread() {
-        @Override
-        public void run() {
+        };
+    };
+    private Runnable placeOnBar() {
+        return () -> {
             try {
-                Log.d("Teamcode", "pob 1 - Expected: GRABW, Actual: " + robot.transfer.stateMachine.getCurrentState());
+                Log.d("Teamcode", "pob 1 - Expected: GRABW, Actual: " +
+                        robot.transfer.stateMachine.getCurrentState());
                 robot.transfer.next();
                 robot.vSlides.moveToPosition(VSlides.LiftPositions.TOP_CHAMBER);
                 robot.vSlides.loop();
-                Log.d("Teamcode", "pob 2 - Expected: SPECIMENSCORE, Actual: " + robot.transfer.stateMachine.getCurrentState());
+                Log.d("Teamcode", "pob 2 - Expected: SPECIMENSCORE, Actual: " +
+                        robot.transfer.stateMachine.getCurrentState());
                 Thread.sleep(1500);
                 robot.vSlides.moveToPosition(VSlides.LiftPositions.BOTTOM_BUCKET);
                 robot.vSlides.loop();
                 robot.transfer.next();
-                Log.d("Teamcode", "pob 3 - Expected: HOVERW, Actual: " + robot.transfer.stateMachine.getCurrentState());
-                synchronized (lock) {
-                    Log.i("Teamcode", "placeOnBar finished, notifying main thread");
-                    lock.notify();
-                }
+                Log.d("Teamcode", "pob 3 - Expected: HOVERW, Actual: " +
+                        robot.transfer.stateMachine.getCurrentState());
             } catch (InterruptedException err) {
-                Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
+                Log.e("Teamcode", "Thread failed; Interrupting Thread...");
+                Thread.currentThread().interrupt();
             }
-        }
-    }, pickOffWall = new Thread() {
-        @Override
-        public void run() {
+        };
+    };
+    private Runnable pickOffWall() {
+        return () -> {
             try {
-                Log.d("Teamcode", "pow 1 - Expected: HOVERW, Actual: " + robot.transfer.stateMachine.getCurrentState());
+                Log.d("Teamcode", "pow 1 - Expected: HOVERW, Actual: " +
+                        robot.transfer.stateMachine.getCurrentState());
                 Thread.sleep(500);
                 robot.vSlides.loop();
                 // Enough to lift it off the wall
                 Thread.sleep(500);
-                synchronized (lock) {
-                    Log.i("Teamcode", "pickOffWall finished, notifying main thread");
-                    lock.notify();
-                }
             } catch (InterruptedException err) {
-                Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
+                Log.e("Teamcode", "Thread failed; Interrupting Thread...");
+                Thread.currentThread().interrupt();
             }
-        }
+        };
     };
 
     public void buildPaths() {
@@ -132,7 +128,7 @@ public class Specimen_1_3_Park extends OpMode {
                     robot.hSlides.moveToPosition(HSlides.SlidePositions.IN);
                     robot.hSlides.loop();
                 })
-                .addParametricCallback(0, pickUpBlock::start)
+                .addParametricCallback(1, () -> executor.submit(this::pickUpBlock))
                 .setLinearHeadingInterpolation(startPose.getHeading(), botSample.getHeading())
                 .build();
 
@@ -142,31 +138,31 @@ public class Specimen_1_3_Park extends OpMode {
                     robot.hSlides.hold();
                     robot.hSlides.loop();
                 })
-                .addParametricCallback(1, dropBlock::start)
+                .addParametricCallback(1, () -> executor.submit(this::dropBlock))
                 .setLinearHeadingInterpolation(botSample.getHeading(), botDrop.getHeading())
                 .build();
 
         drop2mid = follower.pathBuilder()
                 .addPath(new BezierCurve(new Point(botDrop), new Point(midSample)))
-                .addParametricCallback(1, pickUpBlock::start)
+                .addParametricCallback(1, () -> executor.submit(this::pickUpBlock))
                 .setLinearHeadingInterpolation(botDrop.getHeading(), midSample.getHeading())
                 .build();
 
         mid2drop = follower.pathBuilder()
                 .addPath(new BezierCurve(new Point(midSample), new Point(midDrop)))
-                .addParametricCallback(1, dropBlock::start)
+                .addParametricCallback(1, () -> executor.submit(this::dropBlock))
                 .setLinearHeadingInterpolation(midSample.getHeading(), midDrop.getHeading())
                 .build();
 
         drop2top = follower.pathBuilder()
                 .addPath(new BezierCurve(new Point(midDrop), topControl, new Point(topSample)))
-                .addParametricCallback(1, pickUpBlock::start)
+                .addParametricCallback(1, () -> executor.submit(this::pickUpBlock))
                 .setLinearHeadingInterpolation(midDrop.getHeading(), topSample.getHeading())
                 .build();
 
         top2drop = follower.pathBuilder()
                 .addPath(new BezierCurve(new Point(topSample), topControl, new Point(topDrop)))
-                .addParametricCallback(1, dropBlock::start)
+                .addParametricCallback(1, () -> executor.submit(this::dropBlock))
                 .setLinearHeadingInterpolation(topSample.getHeading(), topDrop.getHeading())
                 .build();
     }
@@ -179,75 +175,80 @@ public class Specimen_1_3_Park extends OpMode {
                 break;
             case 1:
                 if (!follower.isBusy()) {
+                    executor.shutdown();
                     try {
-                        synchronized (lock) {
-                            Log.i("Teamcode", "Main thread locking...");
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
+                        if (!executor.awaitTermination(waitTime, TimeUnit.MILLISECONDS)) {
+                            executor.shutdownNow();
                         }
                     } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
+                        Log.e("Teamcode","Timeout reached, forcing shutdown.");
+                        executor.shutdownNow(); // Attempts to stop execution
                     }
+                    executor = Executors.newSingleThreadExecutor();
                     follower.followPath(bot2drop, true);
                     setPathState(2);
                 }
                 break;
             case 2:
                 if (!follower.isBusy()) {
+                    executor.shutdown();
                     try {
-                        synchronized (lock) {
-                            Log.i("Teamcode", "Main thread locking...");
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
+                        if (!executor.awaitTermination(waitTime, TimeUnit.MILLISECONDS)) {
+                            executor.shutdownNow();
                         }
                     } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
+                        Log.e("Teamcode","Timeout reached, forcing shutdown.");
+                        executor.shutdownNow(); // Attempts to stop execution
                     }
+                    executor = Executors.newSingleThreadExecutor();
                     follower.followPath(drop2mid, true);
                     setPathState(3);
                 }
                 break;
             case 4:
                 if (!follower.isBusy()) {
+                    executor.shutdown();
                     try {
-                        synchronized (lock) {
-                            Log.i("Teamcode", "Main thread locking...");
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
+                        if (!executor.awaitTermination(waitTime, TimeUnit.MILLISECONDS)) {
+                            executor.shutdownNow();
                         }
                     } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
+                        Log.e("Teamcode","Timeout reached, forcing shutdown.");
+                        executor.shutdownNow(); // Attempts to stop execution
                     }
+                    executor = Executors.newSingleThreadExecutor();
                     follower.followPath(mid2drop, true);
                     setPathState(5);
                 }
                 break;
             case 5:
                 if (!follower.isBusy()) {
+                    executor.shutdown();
                     try {
-                        synchronized (lock) {
-                            Log.i("Teamcode", "Main thread locking...");
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
+                        if (!executor.awaitTermination(waitTime, TimeUnit.MILLISECONDS)) {
+                            executor.shutdownNow();
                         }
                     } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
+                        Log.e("Teamcode","Timeout reached, forcing shutdown.");
+                        executor.shutdownNow(); // Attempts to stop execution
                     }
+                    executor = Executors.newSingleThreadExecutor();
                     follower.followPath(drop2top, true);
                     setPathState(6);
                 }
                 break;
             case 6:
                 if (!follower.isBusy()) {
+                    executor.shutdown();
                     try {
-                        synchronized (lock) {
-                            Log.i("Teamcode", "Main thread locking...");
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
+                        if (!executor.awaitTermination(waitTime, TimeUnit.MILLISECONDS)) {
+                            executor.shutdownNow();
                         }
                     } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
+                        Log.e("Teamcode","Timeout reached, forcing shutdown.");
+                        executor.shutdownNow(); // Attempts to stop execution
                     }
+                    executor = Executors.newSingleThreadExecutor();
                     follower.followPath(top2drop, true);
                     setPathState(7);
                 }
@@ -325,8 +326,6 @@ public class Specimen_1_3_Park extends OpMode {
      **/
     @Override
     public void stop() {
-        synchronized (lock) {
-            lock.notify();
-        }
+        executor.shutdownNow();
     }
 }
