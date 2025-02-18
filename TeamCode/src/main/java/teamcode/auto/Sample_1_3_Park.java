360370f14e50b90347f2d747695cde8e57b10e1c
Daniel Farkas <54506760+daniel-farkas-dev@users.noreply.github.com>
2025-02-18 08:04:33 -0500

    started auto
    Move the parametric and temporal callbacks to be parallel/ sequential with the FollowPath commands

--- TeamCode/src/main/java/teamcode/auto/Sample_1_3_Park.java
+++ TeamCode/src/main/java/teamcode/auto/Sample_1_3_Park.java
@@ -11,35 +11,32 @@ import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
 import com.pedropathing.util.Constants;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.rowanmcalpin.nextftc.core.command.Command;
+import com.rowanmcalpin.nextftc.core.command.CommandManager;
+import com.rowanmcalpin.nextftc.core.command.groups.SequentialGroup;
+import com.rowanmcalpin.nextftc.pedro.FollowPath;
+import com.rowanmcalpin.nextftc.pedro.PedroOpMode;
 
 import java.util.Arrays;
 
 import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
 import teamcode.hardware.Consts;
-import teamcode.hardware.EndEffector;
-import teamcode.hardware.Intake;
+import teamcode.hardware.HSlides;
 import teamcode.hardware.StateMachine;
+import teamcode.hardware.Transfer;
 import teamcode.hardware.VSlides;
 
 @Autonomous(name = "Sample 1 + 3 W/ Park", group = "Red")
-public class Sample_1_3_Park extends OpMode {
-    private static final Object lock = new Object();
-    private EndEffector ee; private Intake intake;
+public class Sample_1_3_Park extends PedroOpMode {
     public static Pose startPose = new Pose(133, 36, Math.toRadians(90));
     public static Pose basket = new Pose(128.2946175637394, 7.546742209631725, Math.toRadians(135));
     public static Pose topSample = new Pose(113, 13.5, Math.toRadians(180));
     public static Pose midSample = new Pose(113.5, 3.5, Math.toRadians(180));
     public static Pose botSample = new Pose(114.5, 3, Math.toRadians(230));
     public static Pose botBackup = new Pose(120, 10, Math.toRadians(230));
-//    public static Pose park1 = new Pose(83, 12, Math.toRadians(90));
-//    public static Pose park2 = new Pose(83, 45, Math.toRadians(90));
-    private static int pathState = 0;
-    public Robot robot = Robot.getInstance();
-    private Follower follower;
-    private PathChain preload2basket, basket2top, top2basket, basket2mid, mid2basket, basket2bot,
-            bot2basket, basket2park/*, park2start*/;
+    public static Pose park1 = new Pose(83, 12, Math.toRadians(90));
+    public static Pose park2 = new Pose(83, 45, Math.toRadians(90));
     private final Thread placeInBucket = new Thread() {
         @Override
         public void run() {
@@ -98,6 +95,11 @@ public class Sample_1_3_Park extends OpMode {
             }
         }
     };
+    private PathChain preload2basket, basket2top, top2basket, basket2mid, mid2basket, basket2bot,
+            bot2basket, basket2park, park2start;
+    public Sample_1_3_Park() {
+        super(Transfer.INSTANCE, VSlides.INSTANCE, HSlides.INSTANCE);
+    }
 
     public void buildPaths() {
         // NOTES:
@@ -105,6 +107,7 @@ public class Sample_1_3_Park extends OpMode {
         // * putInBasket takes you from TRANSFER to BARRIER
         preload2basket = follower.pathBuilder()
                 .addPath(new Path(new BezierLine(new Point(startPose), new Point(basket))))
+                .setLinearHeadingInterpolation(startPose.getHeading(), basket.getHeading())
                 .addParametricCallback(0, () -> {
                     Log.d("Teamcode", "p2b started");
                     new Thread() {
@@ -129,56 +132,56 @@ public class Sample_1_3_Park extends OpMode {
                         }
                     }.start();
                 })
-                .setLinearHeadingInterpolation(startPose.getHeading(), basket.getHeading())
                 .build();
 
         basket2top = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(basket), new Point(topSample)))
+                .setLinearHeadingInterpolation(basket.getHeading(), topSample.getHeading())
                 .addParametricCallback(0, () -> {
                     Log.d("Teamcode", "b2t 1 - Expected: START; Actual: " + robot.transfer.stateMachine.getCurrentState());
                     robot.transfer.next(); // Move from START to BARRIER
                     Log.d("Teamcode", "b2t 2 - Expected: BARRIER; Actual: " + robot.transfer.stateMachine.getCurrentState());
                     pickUpBlock.start(); // Go to TRANSFER
                 })
-                .setLinearHeadingInterpolation(basket.getHeading(), topSample.getHeading())
                 .build();
 
         top2basket = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(topSample), new Point(basket)))
-                .addParametricCallback(0, placeInBucket::start)
                 .setLinearHeadingInterpolation(topSample.getHeading(), basket.getHeading())
+                .addParametricCallback(0, placeInBucket::start)
                 .build();
 
         basket2mid = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(basket), new Point(midSample)))
-                .addParametricCallback(0, pickUpBlock::start)
                 .setLinearHeadingInterpolation(basket.getHeading(), midSample.getHeading())
+                .addParametricCallback(0, pickUpBlock::start)
                 .build();
 
         mid2basket = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(midSample), new Point(basket)))
-                .addParametricCallback(0, placeInBucket::start)
                 .setLinearHeadingInterpolation(midSample.getHeading(), basket.getHeading())
+                .addParametricCallback(0, placeInBucket::start)
                 .build();
 
         basket2bot = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(basket), new Point(botSample)))
-                .addParametricCallback(0, pickUpBlock::start)
                 .setLinearHeadingInterpolation(basket.getHeading(), botSample.getHeading())
+                .addParametricCallback(0, pickUpBlock::start)
                 .build();
 
         bot2basket = follower.pathBuilder()
                 .addPath(new BezierCurve(new Point(botSample), new Point(botBackup)))
                 .setLinearHeadingInterpolation(botSample.getHeading(), botBackup.getHeading())
                 .addPath(new BezierLine(new Point(botBackup), new Point(basket)))
-                .addParametricCallback(0, placeInBucket::start)
                 .setLinearHeadingInterpolation(botBackup.getHeading(), basket.getHeading())
+                .addParametricCallback(0, placeInBucket::start)
                 .build();
 
         basket2park = follower.pathBuilder()
                 .addPath(new BezierCurve(new Point(basket), new Point(topSample)))
                 .addParametricCallback(0, () -> {
-                   robot.transfer.next(); robot.vSlides.moveToPosition(VSlides.LiftPositions.BOTTOM);
+                    robot.transfer.next();
+                    robot.vSlides.moveToPosition(VSlides.LiftPositions.BOTTOM);
                     robot.vSlides.loop();
                 })
                 .setLinearHeadingInterpolation(basket.getHeading(), topSample.getHeading())
@@ -186,161 +189,32 @@ public class Sample_1_3_Park extends OpMode {
 //                .setLinearHeadingInterpolation(park1.getHeading(), park2.getHeading())
                 .build();
 
-//        park2start = follower.pathBuilder()
-//                .addPath(new BezierCurve(new Point(park2), new Point(park1)))
-//                .setLinearHeadingInterpolation(park2.getHeading(), park1.getHeading())
-//                .addPath(new BezierCurve(new Point(park1), new Point(startPose)))
-//                .setLinearHeadingInterpolation(park1.getHeading(), startPose.getHeading())
-//                .build();
+        park2start = follower.pathBuilder()
+                .addPath(new BezierCurve(new Point(park2), new Point(park1)))
+                .setLinearHeadingInterpolation(park2.getHeading(), park1.getHeading())
+                .addPath(new BezierCurve(new Point(park1), new Point(startPose)))
+                .setLinearHeadingInterpolation(park1.getHeading(), startPose.getHeading())
+                .build();
     }
 
-    public void autonomousPathUpdate() {
-        switch (pathState) {
-            case 0:
-                follower.followPath(preload2basket);
-                setPathState(1);
-                break;
-            case 1:
-                if (!follower.isBusy()) {
-                    try {
-                        Log.i("Teamcode", "Main thread locking...");
-                        synchronized (lock) {
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
-                        }
-                    } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
-                    }
-                    follower.followPath(basket2top, true);
-                    setPathState(2);
-                }
-                break;
-            case 2:
-                if (!follower.isBusy()) {
-                    try {
-                        Log.i("Teamcode", "Main thread locking...");
-                        synchronized (lock) {
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
-                        }
-                    } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
-                    }
-                    follower.followPath(top2basket, true);
-                    setPathState(3);
-                }
-                break;
-            case 3:
-                if (!follower.isBusy()) {
-                    try {
-                        Log.i("Teamcode", "Main thread locking...");
-                        synchronized (lock) {
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
-                        }
-                    } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
-                    }
-                    follower.followPath(basket2mid, true);
-                    setPathState(4);
-                }
-                break;
-            case 4:
-                if (!follower.isBusy()) {
-                    try {
-                        Log.i("Teamcode", "Main thread locking...");
-                        synchronized (lock) {
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
-                        }
-                    } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
-                    }
-                    follower.followPath(mid2basket, true);
-                    setPathState(5);
-                }
-                break;
-            case 5:
-                if (!follower.isBusy()) {
-                    try {
-                        Log.i("Teamcode", "Main thread locking...");
-                        synchronized (lock) {
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
-                        }
-                    } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
-                    }
-                    follower.followPath(basket2bot, true);
-                    setPathState(6);
-                }
-                break;
-            case 6:
-                if (!follower.isBusy()) {
-                    try {
-                        Log.i("Teamcode", "Main thread locking...");
-                        synchronized (lock) {
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
-                        }
-                    } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
-                    }
-                    follower.followPath(bot2basket, true);
-                    setPathState(7);
-                }
-                break;
-            case 7:
-                if (!follower.isBusy()) {
-                    try {
-                        Log.i("Teamcode", "Main thread locking...");
-                        synchronized (lock) {
-                            lock.wait();
-                            Log.i("Teamcode", "Main thread notified");
-                        }
-                    } catch (InterruptedException err) {
-                        Log.e("Failed to handle multi threading{}", Arrays.toString(err.getStackTrace()));
-                    }
-                    follower.followPath(basket2park, true);
-                    setPathState(8);
-                }
-//            case 8:
-//                if (!follower.isBusy()) {
-//                    follower.followPath(park2start, true);
-//                    setPathState(9);
-//                }
-            case 8:
-                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
-                if (!follower.isBusy()) {
-                    /* Set the state to a Case we won't use or define, so it just stops running an new paths */
-                    setPathState(-1);
-                }
-                break;
-        }
+    public Command routine() {
+        return new SequentialGroup(
+                new FollowPath(preload2basket),
+                new FollowPath(basket2top, true),
+                new FollowPath(top2basket, true),
+                new FollowPath(basket2mid, true),
+                new FollowPath(mid2basket, true),
+                new FollowPath(basket2bot, true),
+                new FollowPath(bot2basket, true),
+                new FollowPath(basket2park, true),
+                new FollowPath(park2start, true),
+                );
     }
 
-
-    public void setPathState(int pState) {
-        pathState = pState;
-    }
-
-    /**
-     * This is the main loop of the OpMode, it will run repeatedly after clicking "Play".
-     **/
     @Override
-    public void loop() {
-
-        // These loop the movements of the robot
-        follower.update();
-        autonomousPathUpdate();
-
-        StateMachine.State state = robot.transfer.stateMachine.getCurrentState();
-
-        robot.hSlides.hold();
-        robot.hSlides.loop();
-
-        // Feedback to Driver Hub
-        telemetry.addData("path state", pathState);
+    public void onUpdate() {
+        StateMachine.State state = Transfer.INSTANCE.stateMachine.getCurrentState();
+        telemetry.addData("Commands", CommandManager.INSTANCE.getRunningCommands().toString());
         telemetry.addData("State", state.name());
         telemetry.addData("x", follower.getPose().getX());
         telemetry.addData("y", follower.getPose().getY());
@@ -352,25 +226,19 @@ public class Sample_1_3_Park extends OpMode {
      * This method is called once at the init of the OpMode.
      **/
     @Override
-    public void init() {
-        robot.init(hardwareMap);
-        robot.transfer.flush();
-        Log.d("Teamcode", "Entered init callback");
+    public void onInit() {
         Constants.setConstants(FConstants.class, LConstants.class);
         follower = new Follower(hardwareMap);
-        follower.setStartingPose(startPose);
+        follower.setStartingPose(new Pose(0, 0, 0));
         buildPaths();
-
-        intake = robot.transfer.intake; ee = robot.transfer.ee;
-
-        ee.setClaw(Consts.E_CLAW_CLOSE);
     }
 
     /**
      * This method is called continuously after Init while waiting for "play".
+     * Use this to collect info from the gamepad to change variables.
      **/
     @Override
-    public void init_loop() {
+    public void onWaitForStart() {
     }
 
     /**
@@ -378,20 +246,15 @@ public class Sample_1_3_Park extends OpMode {
      * It runs all the setup actions, including building paths and starting the path system
      **/
     @Override
-    public void start() {
-        robot.vSlides.moveToPosition(VSlides.LiftPositions.TOP_BUCKET);
-        robot.vSlides.loop();
-        setPathState(0);
+    public void onStartButtonPressed() {
+        routine().invoke();
     }
 
     /**
      * We do not use this because everything should automatically disable
      **/
     @Override
-    public void stop() {
-        synchronized (lock) {
-            lock.notify();
-        }
+    public void onStop() {
     }
 
 }
